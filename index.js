660bf363084496d2e27e4238d6b36a9d67810b79
Aarif mohammad <haidarali4408@gmail.com>
2022-10-29 10:03:31 +0530

    ssss

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ import categorys from "./router/Category.router.js";
 import productRouter from "./router/product.router.js";
 import sbcategory from "./router/sbcategory.router.js";
 import router from "./router/user.route.js";
-
+import { config } from "dotenv";
 
 const app = express();
 app.use(express.json())
 connectiontb()
-
+config();
 app.use(router)
 app.use(categorys)
 app.use(productRouter)
